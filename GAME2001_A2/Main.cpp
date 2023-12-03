96dbf148a99269e5ed4587584abd577cd1166f81
JulianYao1 <115187550+JulianYao1@users.noreply.github.com>
2023-12-03 15:03:15 -0500

    tweaked NetWorkMessage to be generic

--- GAME2001_A2/Main.cpp
+++ GAME2001_A2/Main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template<class T>
-class less_cmp // Less than compare with nonpointer type
+class less_cmp 
 {
 public:
 	inline bool operator()(T lVal, T rVal)
@@ -13,103 +13,95 @@ public:
 	}
 };
 
-template<class T>
-class less_cmp_pointer // Less than compare with pointer types
+template <class T>
+class PriorityQueueData
 {
 public:
-	inline bool operator()(T lVal, T rVal)
-	{
-		return ((*lVal) < (*rVal));
-	}
-};
-
-template<class T>
-class greater_cmp // Less than compare with nonpointer type
-{
-public:
-	inline bool operator()(T lVal, T rVal)
-	{
-		return !(lVal < rVal);
-	}
-};
-
-template<class T>
-class greater_cmp_pointer // Less than compare with pointer types
-{
-public:
-	inline bool operator()(T lVal, T rVal)
-	{
-		return !((*lVal) < (*rVal));
-	}
-};
-
-class NetworkMessage
-{
-public:
-	NetworkMessage() : m_priority(0), m_id(0) {}
-	NetworkMessage(int p, int id) : m_priority(p), m_id(id) {}
-	~NetworkMessage() {}
+	PriorityQueueData() : m_priority(0), m_data() {}
+	PriorityQueueData(int p, T data) : m_priority(p), m_data(data) {}
+	~PriorityQueueData() {}
 
 	int GetPriority()
 	{
 		return m_priority;
 	}
-	int GetID()
+	T GetData()
 	{
-		return m_id;
+		return m_data;
 	}
 
-	// Overload operator
-	bool operator<(NetworkMessage& m)
+	bool operator<(PriorityQueueData& m)
 	{
-		// Check the priority level of both network messages
 		if (m_priority < m.GetPriority())
-		{
-			return true;
-		}
-		else if (m_id < m.GetID())
 		{
 			return true;
 		}
 
 		return false;
 	}
-	bool operator>(NetworkMessage& m)
+	bool operator>(PriorityQueueData& m)
 	{
-		return !(*this < m);	// NOT( this is less than m )
+		return !(*this < m);	
 	}
 private:
 	int m_priority;
-	int m_id;
+	T m_data;
 };
 
 int main()
 {
 	cout << "Priority queue data structure example!" << endl << endl;
 
-	const int SIZE = 4;
-	PriorityQueue<NetworkMessage, less_cmp<NetworkMessage>> que(SIZE);
+	const int SIZE = 5;
+	PriorityQueue<PriorityQueueData<string>, less_cmp<PriorityQueueData<string>>> stringQue(SIZE);
+	PriorityQueue<PriorityQueueData<int>, less_cmp<PriorityQueueData<int>>> intQue(SIZE);
 
-	que.push(NetworkMessage(3, 100));
-	que.push(NetworkMessage(2, 286));
-	que.push(NetworkMessage(1, 362));
-	que.push(NetworkMessage(3, 435));
+	stringQue.push(PriorityQueueData<string>(5, "String1"));
+	stringQue.push(PriorityQueueData<string>(1, "String2"));
+	stringQue.push(PriorityQueueData<string>(3, "String3"));
+	stringQue.push(PriorityQueueData<string>(2, "String4"));
+	stringQue.push(PriorityQueueData<string>(4, "String5"));
+
+	intQue.push(PriorityQueueData<int>(5, 1));
+	intQue.push(PriorityQueueData<int>(1, 2));
+	intQue.push(PriorityQueueData<int>(3, 3));
+	intQue.push(PriorityQueueData<int>(2, 4));
+	intQue.push(PriorityQueueData<int>(4, 5));
 
 	// Display the priority queue
-	cout << "Priority queue content (Size - " << que.GetSize() << ") : " << endl;
+	cout << "Priority queue content (Size - " << stringQue.GetSize() << ") : " << endl;
 
-	while (que.isEmpty() == false)
+	while (stringQue.isEmpty() == false)
 	{
-		cout << "  Priority: " << que.front().GetPriority();
-		cout << "  ID: " << que.front().GetID();
+		cout << "  Priority: " << stringQue.front().GetPriority();
+		cout << "  ID: " << stringQue.front().GetData();
 		cout << endl;
 
-		que.pop();
+		stringQue.pop();
+	}
+	cout << endl;
+	if (stringQue.isEmpty() == true)
+	{
+		cout << "The container is empty." << endl;
+	}
+	else
+	{
+		cout << "The container is NOT empty." << endl;
 	}
 
-	cout << endl;
+	// Display the priority queue
+	cout << "Priority queue content (Size - " << intQue.GetSize() << ") : " << endl;
 
-	if (que.isEmpty() == true)
+	while (intQue.isEmpty() == false)
+	{
+		cout << "  Priority: " << intQue.front().GetPriority();
+		cout << "  ID: " << intQue.front().GetData();
+		cout << endl;
+
+		intQue.pop();
+	}
+	cout << endl;
+	if (intQue.isEmpty() == true)
 	{
 		cout << "The container is empty." << endl;
 	}
